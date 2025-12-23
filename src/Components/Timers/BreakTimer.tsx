2d570bab4e8c338bdc135085cddf2dd9819200d4
Tyler Hilbig <bighilltyler@gmail.com>
2025-12-22 19:28:01 -0700

    Added basic code, lots of placeholders. Just getting some visual feedback to play with while we build.

---
+++ src/Components/Timers/BreakTimer.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./BreakTimer.css";
+
+const BreakTimer: React.FC = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+
+  useEffect(() => {
+    if (!isRunning) return;
+
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const formatTime = (s: number) => {
+    const min = Math.floor(s / 60);
+    const sec = s % 60;
+    return `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Break</h1>
+        <div className="timer">{formatTime(seconds)}</div>
+      </div>
+    </>
+  );
+};
+
+export default BreakTimer;
---
+++ src/Components/TodoList/TodoList.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+
+
+const TodoList: React.FC = () => {
+return <div className="todo-list">{}</div>;
+};
+
+
+
+
+
+
+
+export default TodoList;
